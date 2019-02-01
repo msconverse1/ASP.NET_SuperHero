7854663536b1b768ec5586ab6b0901f67c51d868
msconverse1 <msconverse@fullsail.edu>
2019-02-01 11:51:33 -0600

    Create and View
    Made Functionallity that allows the user to enter information about the
    superheroes that they want to add
    
    Allows the user the ability to view all the Heroes in the table

--- SuperHero/SuperHero/Controllers/SuperHeroController.cs
+++ SuperHero/SuperHero/Controllers/SuperHeroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperHero.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,11 @@ namespace SuperHero.Controllers
 {
     public class SuperHeroController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: SuperHero
         public ActionResult Index()
         {
-            return View();
+            return View(db.SuperHeroes);
         }
 
         // GET: SuperHero/Details/5
@@ -23,17 +25,23 @@ namespace SuperHero.Controllers
         // GET: SuperHero/Create
         public ActionResult Create()
         {
+                
             return View();
         }
 
         // POST: SuperHero/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(SuperHeroes superHeroes)
         {
             try
             {
                 // TODO: Add insert logic here
-
+                if (ModelState.IsValid)
+                {
+                    db.SuperHeroes.Add(superHeroes);
+                    db.SaveChanges();
+                   
+                }
                 return RedirectToAction("Index");
             }
             catch
