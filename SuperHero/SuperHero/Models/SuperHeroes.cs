d2cc6c45239df4c2d57ec4a43e339b69e853bff1
msconverse1 <msconverse@fullsail.edu>
2019-02-01 10:35:48 -0600

    Project Setup

---
+++ SuperHero/SuperHero/Models/SuperHeroes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHero.Models
+{
+    public class SuperHeroes
+    {
+        [Key]
+        public int ID { get; set; }
+        public string HeroName { get; set; }
+        public string AlterEgoName { get; set; }
+        public string MainAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+    }
+}
